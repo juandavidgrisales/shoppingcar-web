9bd9901d651c15b4194ef362a33ebca109747b9a
Juan David Grisales Garzon <juan.grisales@globant.com>
2016-11-06 22:54:20 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+	include('header.html');
+
+	include('home.html');
+
+	/*$servername = "127.0.0.1";
+	$username = "arotexconfeccion";
+	$password = "Arotex190";
+	$dbname = "arotexco_shoppingcar";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname, 3306);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+
+	$sql = "SELECT * FROM company";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        echo "id: " . $row["id"]. " - Name: " . $row["description"]. "<br>";
+	    }
+	} else {
+	    echo "0 results";
+	}
+	$conn->close();*/
+
+	include('footer.html');
+?>
