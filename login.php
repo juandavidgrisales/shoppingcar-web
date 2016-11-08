d792c52552f36cbf99d7e1841f2b6a577a2cbb8c
Jefferson Gaviria Aranzalez <Jefferson Gaviria Aranzalez>
2016-11-08 18:21:45 -0500

    agregar camino al login

--- login.php
+++ login.php
@@ -30,7 +30,13 @@
 	}
 ?>
 	<section id="form" style="margin-top: 0px;"><!--form-->
-		<div class="container">
+			<div class="container">
+			<div class="breadcrumbs">
+				<ol class="breadcrumb">
+					<li><a href="#">Home</a></li>
+					<li class="active">Login</li>
+				</ol>
+			</div>
 			<div class="row">
 				<div class="col-sm-4 col-sm-offset-1">
 					<div class="login-form"><!--login form-->
