0a9b1cbdeea650b141f9346ee7bb1921661af8c4
Juan David Grisales Garzón <juandavid.grisales@gmail.com>
2016-11-08 13:41:28 -0500

    dinamic product details

--- product-details.php
+++ product-details.php
@@ -1,105 +1,128 @@
 <?php
-	ob_start();
-	include('session.php');
-	include('header.php');
+include('session.php');
+include('header.php');
 ?>
+<script type="text/javascript">
+	$("#dama-ropa2").click(function(){
+		$("#img1").attr("src","images/product-details/sastreformal2piezas/grandes/View3.jpg");//.on("load",function(){});});
+		$("#img2").attr("src","images/product-details/sastreformal2piezas/grandes/View3.jpg");
+		$("#img3").attr("src","images/product-details/sastreformal2piezas/grandes/View3.jpg");
+		$("#img4").attr("src","images/product-details/sastreformal2piezas/grandes/View3.jpg");
+	});
+</script>
 
 
 <section>
 
-		<div class="container">
-				<div class="breadcrumbs">
-					<ol class="breadcrumb">
-					  <li><a href="#">Home</a></li>
-					  <li class="active">Productos</li>
-					</ol>
-				</div>
-			<div class="row">
-					<div class="col-sm-3">
-						<div class="left-sidebar">
-							<h2>Segmentos</h2>
-							<div class="panel-group category-products" id="accordian"><!--category-productsr-->
-								<div class="panel panel-default">
-									<div class="panel-heading">
-										<h4 class="panel-title">
-											<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
-												<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-												Ropa Dama
-											</a>
-										</h4>
-									</div>
-									<div id="sportswear" class="panel-collapse collapse">
-										<div class="panel-body">
-											<ul>
-												<li><a href="#">-Sastre formal de dos (2) piezas</a></li>
-												<li><a href="#">-Blusa formal manga larga</a></li>
-											</ul>
-										</div>
-									</div>
-								</div>
-								<div class="panel panel-default">
-									<div class="panel-heading">
-										<h4 class="panel-title">
-											<a data-toggle="collapse" data-parent="#accordian" href="#mens">
-												<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-												Ropa Caballero
-											</a>
-										</h4>
-									</div>
-									<div id="mens" class="panel-collapse collapse">
-										<div class="panel-body">
-											<ul>
-												<li><a href="#">-Vestido formal de dos (2) piezas gama alta</a></li>
-												<li><a href="#">-Camisa formal manga larga</a></li>
-												<li><a href="#">-Corbata</a></li>
-											</ul>
-										</div>
-									</div>
-								</div>
-								
-								<div class="panel panel-default">
-									<div class="panel-heading">
-										<h4 class="panel-title">
-											<a data-toggle="collapse" data-parent="#accordian" href="#womens">
-												<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-												Calzado Dama
-											</a>
-										</h4>
-									</div>
-									<div id="womens" class="panel-collapse collapse">
-										<div class="panel-body">
-											<ul>
-												<li><a href="#">-Diseño Clasico y de Moda</a></li>
-												<li><a href="#">-Diseño Deportivo</a></li>
-											</ul>
-										</div>
-									</div>
-								</div>
+	<div class="container">
 
-								<div class="panel panel-default">
-									<div class="panel-heading">
-										<h4 class="panel-title">
-											<a data-toggle="collapse" data-parent="#accordian" href="#womens2">
-												<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-												Calzado Caballero
-											</a>
-										</h4>
-									</div>
-									<div id="womens2" class="panel-collapse collapse">
-										<div class="panel-body">
-											<ul>
-												<li><a href="#">-Diseño Clasico y de Moda</a></li>
-												<li><a href="#">-Diseño Deportivo</a></li>
-												<li><a href="#">-Cinturon</a></li>
-											</ul>
-										</div>
-									</div>
+		<div class="breadcrumbs">
+			<ol class="breadcrumb">
+				<li><a href="#">Home</a></li>
+				<li class="active">Productos</li>
+			</ol>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="left-sidebar">
+					<h2>Segmentos</h2>
+					<div class="panel-group category-products" id="accordian"><!--category-productsr-->
+						<?php
+						if ($user_check=='38228444' || $user_check=='38228444') {
+						?>
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h4 class="panel-title">
+									<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
+										<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+										Ropa Dama
+									</a>
+								</h4>
+							</div>
+							<div id="sportswear" class="panel-collapse collapse">
+								<div class="panel-body">
+									<ul>
+										<li><a id="dama-ropa1" href="#">-Sastre formal de dos (2) piezas</a></li>
+										<li><a id="dama-ropa2" href="#">-Blusa formal manga larga</a></li>
+									</ul>
 								</div>
-							</div><!--/category-products-->
+							</div>
+						</div>
+						<?php
+						}
+						if ($user_check=='14225334' || $user_check=='93367444') {
+						?>
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h4 class="panel-title">
+									<a data-toggle="collapse" data-parent="#accordian" href="#mens">
+										<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+										Ropa Caballero
+									</a>
+								</h4>
+							</div>
+							<div id="mens" class="panel-collapse collapse">
+								<div class="panel-body">
+									<ul>
+										<li><a href="#">-Vestido formal de dos (2) piezas gama alta</a></li>
+										<li><a href="#">-Camisa formal manga larga</a></li>
+										<li><a href="#">-Corbata</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<?php
+						}
+						if ($user_check=='38228444' || $user_check=='38555333') {
+						?>
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h4 class="panel-title">
+									<a data-toggle="collapse" data-parent="#accordian" href="#womens">
+										<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+										Calzado Dama
+									</a>
+								</h4>
+							</div>
+							<div id="womens" class="panel-collapse collapse">
+								<div class="panel-body">
+									<ul>
+										<li><a href="#">-Diseño Clasico y de Moda</a></li>
+										<li><a href="#">-Diseño Deportivo</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<?php
+						}
+						if ($user_check=='14225334' || $user_check=='93367444') {
+						?>
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h4 class="panel-title">
+									<a data-toggle="collapse" data-parent="#accordian" href="#womens2">
+										<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+										Calzado Caballero
+									</a>
+								</h4>
+							</div>
+							<div id="womens2" class="panel-collapse collapse">
+								<div class="panel-body">
+									<ul>
+										<li><a href="#">-Diseño Clasico y de Moda</a></li>
+										<li><a href="#">-Diseño Deportivo</a></li>
+										<li><a href="#">-Cinturon</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<?php
+						}
+						?>
+					</div><!--/category-products-->
 
-					
-
-						<div class="brands_products"><!--brands_products-->
+					<div class="brands_products"><!--brands_products-->
 								<!--<h2>Brands</h2>
 								<div class="brands-name">
 									<ul class="nav nav-pills nav-stacked">
@@ -114,87 +137,87 @@
 								</div>-->
 							</div><!--/brands_products-->
 
-						
 
-						<div class="price-range"><!--price-range-->
+
+							<div class="price-range"><!--price-range-->
 								<!--<h2>Price Range</h2>
 								<div class="well text-center">
 									 <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2" ><br />
 									 <b class="pull-left">$ 0</b> <b class="pull-right">$ 600</b>
-								</div>-->
-							</div><!--/price-range-->
+									</div>-->
+								</div><!--/price-range-->
 
-						
 
-						<div class="shipping text-center"><!--shipping-->
-								<!--<img src="images/home/shipping.jpg" alt="" />-->
-							</div><!--/shipping-->
 
-						
+								<div class="shipping text-center"><!--shipping-->
+									<!--<img src="images/home/shipping.jpg" alt="" />-->
+								</div><!--/shipping-->
 
-					</div>
 
-				</div>
 
-				
-
-				<div class="col-sm-9 padding-right">
-
-					<div class="product-details"><!--product-details-->
-
-						<div class="col-sm-5">
-						<div id="similar-product" class="carousel slide" data-ride="carousel">
-
-							<div class="carousel-inner">
-								<div class="item active">
-								<img src="images/product-details/sastreformal2piezas/grandes/View3.jpg" alt="" />
-								</div>
-
-								<div class="item">
-								<img src="images/product-details/sastreformal2piezas/grandes/View2.jpg" alt="" />
-								</div>
-
-								<div class="item">
-								<img src="images/product-details/sastreformal2piezas/grandes/View1.jpg" alt="" />
-								</div>
-
-								<div class="item">
-								<img src="images/product-details/sastreformal2piezas/grandes/View4.jpg" alt="" />
-								</div>
 							</div>
 
-							<!-- Controls -->
-
-								  <a class="left item-control" href="#similar-product" data-slide="prev">
-
-									<i class="fa fa-angle-left"></i>
-
-								  </a>
-
-								  <a class="right item-control" href="#similar-product" data-slide="next">
-
-									<i class="fa fa-angle-right"></i>
-
-								  </a>
 						</div>
 
-							<div id="similar-product" data-ride="carousel">
 
-									<!-- Wrapper for slides -->
 
-								    <div class="carousel-inner">
+						<div class="col-sm-9 padding-right">
 
-										<div class="item active">
+							<div class="product-details"><!--product-details-->
 
-										  <a><img src="images/product-details/sastreformal2piezas/miniaturas/View3.jpg" alt=""></a>
+								<div class="col-sm-5">
+									<div id="similar-product" class="carousel slide" data-ride="carousel">
 
-										  <a><img src="images/product-details/sastreformal2piezas/miniaturas/View2.jpg" alt=""></a>
+										<div class="carousel-inner">
+											<div  id="img1" class="item active">
+												<img src="images/product-details/sastreformal2piezas/grandes/View3.jpg" alt="" />
+											</div>
 
-										  <a><img src="images/product-details/sastreformal2piezas/miniaturas/View1.jpg" alt=""></a>
+											<div id="img2" class="item">
+												<img src="images/product-details/sastreformal2piezas/grandes/View2.jpg" alt="" />
+											</div>
 
-										  <a><img src="images/product-details/sastreformal2piezas/miniaturas/View4.jpg" alt=""></a>
+											<div id="img3" class="item">
+												<img src="images/product-details/sastreformal2piezas/grandes/View1.jpg" alt="" />
+											</div>
 
+											<div id="img4" class="item">
+												<img src="images/product-details/sastreformal2piezas/grandes/View4.jpg" alt="" />
+											</div>
 										</div>
+
+										<!-- Controls -->
+
+										<a class="left item-control" href="#similar-product" data-slide="prev">
+
+											<i class="fa fa-angle-left"></i>
+
+										</a>
+
+										<a class="right item-control" href="#similar-product" data-slide="next">
+
+											<i class="fa fa-angle-right"></i>
+
+										</a>
+									</div>
+
+									<div id="similar-product" data-ride="carousel">
+
+										<!-- Wrapper for slides -->
+
+										<div class="carousel-inner">
+
+											<div class="item active">
+
+												<a><img id="img1-mini" src="images/product-details/sastreformal2piezas/miniaturas/View3.jpg" alt=""></a>
+
+												<a><img id="img2-mini" src="images/product-details/sastreformal2piezas/miniaturas/View2.jpg" alt=""></a>
+
+												<a><img id="img3-mini" src="images/product-details/sastreformal2piezas/miniaturas/View1.jpg" alt=""></a>
+
+												<a><img id="img4-mini" src="images/product-details/sastreformal2piezas/miniaturas/View4.jpg" alt=""></a>
+
+											</div>
 
 										<!--<div class="item">
 
@@ -226,7 +249,7 @@
 
 
 
-								  <!-- Controls -->
+									<!-- Controls -->
 
 								  <!--<a class="left item-control" href="#similar-product" data-slide="prev">
 
@@ -238,7 +261,7 @@
 
 									<i class="fa fa-angle-right"></i>
 
-								  </a>-->
+								</a>-->
 
 							</div>
 
@@ -280,22 +303,22 @@
 
 								<p><b>Talla</b></p>
 								<select class="col-sm-3" name="color">
-								    <option value="S">XS</option>
-								    <option value="S">S</option>
-								    <option value="M">M</option>
-								    <option value="L">L</option>
-								    <option value="XL">XL</option>
-								    <option value="2XL">2XL</option>
-								    <option value="3XL">3XL</option>
-								 </select>
+									<option value="S">XS</option>
+									<option value="S">S</option>
+									<option value="M">M</option>
+									<option value="L">L</option>
+									<option value="XL">XL</option>
+									<option value="2XL">2XL</option>
+									<option value="3XL">3XL</option>
+								</select>
 								<br></br>
 								<p><b>Color</b></p>
 								<select class="col-sm-3" name="color">
-								    <option value="cafe">Café</option>
-								    <option value="Negro">Negro</option>
-								    <option value="Vinotinto">Vinotinto</option>
-								    <option value="Azul">Azul</option>
-								 </select>
+									<option value="cafe">Café</option>
+									<option value="Negro">Negro</option>
+									<option value="Vinotinto">Vinotinto</option>
+									<option value="Azul">Azul</option>
+								</select>
 
 								<!--<a href=""><img src="images/product-details/share.png" class="share img-responsive"  alt="" /></a>-->
 
@@ -695,7 +718,6 @@
 
 	</section>
 
-<?php
+	<?php
 	include('footer.php');
-	ob_end_flush();
-?>	
+	?>	
