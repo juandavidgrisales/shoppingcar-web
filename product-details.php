ff5274a79fe9314049c0c783f6b170462c07615d
Jefferson Gaviria Aranzalez <Jefferson Gaviria Aranzalez>
2016-11-07 22:02:52 -0500

    Cambios product details2

--- product-details.php
+++ product-details.php
@@ -143,60 +143,27 @@
 					<div class="product-details"><!--product-details-->
 
 						<div class="col-sm-5">
+						<div id="similar-product" class="carousel slide" data-ride="carousel">
 
-							<div class="view-product">
+							<div class="carousel-inner">
+								<div class="item active">
+								<img src="images/product-details/sastreformal2piezas/grandes/View3.jpg" alt="" />
+								</div>
 
-								<img src="images/product-details/1.jpg" alt="" />
+								<div class="item">
+								<img src="images/product-details/sastreformal2piezas/grandes/View2.jpg" alt="" />
+								</div>
 
-								<!--<h3>ZOOM</h3>-->
+								<div class="item">
+								<img src="images/product-details/sastreformal2piezas/grandes/View1.jpg" alt="" />
+								</div>
 
+								<div class="item">
+								<img src="images/product-details/sastreformal2piezas/grandes/View4.jpg" alt="" />
+								</div>
 							</div>
 
-							<div id="similar-product" class="carousel slide" data-ride="carousel">
-
-								
-
-								  <!-- Wrapper for slides -->
-
-								    <div class="carousel-inner">
-
-										<div class="item active">
-
-										  <a href=""><img src="images/product-details/similar1.jpg" alt=""></a>
-
-										  <a href=""><img src="images/product-details/similar2.jpg" alt=""></a>
-
-										  <a href=""><img src="images/product-details/similar3.jpg" alt=""></a>
-
-										</div>
-
-										<div class="item">
-
-										  <a href=""><img src="images/product-details/similar1.jpg" alt=""></a>
-
-										  <a href=""><img src="images/product-details/similar2.jpg" alt=""></a>
-
-										  <a href=""><img src="images/product-details/similar3.jpg" alt=""></a>
-
-										</div>
-
-										<div class="item">
-
-										  <a href=""><img src="images/product-details/similar1.jpg" alt=""></a>
-
-										  <a href=""><img src="images/product-details/similar2.jpg" alt=""></a>
-
-										  <a href=""><img src="images/product-details/similar3.jpg" alt=""></a>
-
-										</div>
-
-										
-
-									</div>
-
-
-
-								  <!-- Controls -->
+							<!-- Controls -->
 
 								  <a class="left item-control" href="#similar-product" data-slide="prev">
 
@@ -209,6 +176,69 @@
 									<i class="fa fa-angle-right"></i>
 
 								  </a>
+						</div>
+
+							<div id="similar-product" data-ride="carousel">
+
+									<!-- Wrapper for slides -->
+
+								    <div class="carousel-inner">
+
+										<div class="item active">
+
+										  <a><img src="images/product-details/sastreformal2piezas/miniaturas/View3.jpg" alt=""></a>
+
+										  <a><img src="images/product-details/sastreformal2piezas/miniaturas/View2.jpg" alt=""></a>
+
+										  <a><img src="images/product-details/sastreformal2piezas/miniaturas/View1.jpg" alt=""></a>
+
+										  <a><img src="images/product-details/sastreformal2piezas/miniaturas/View4.jpg" alt=""></a>
+
+										</div>
+
+										<!--<div class="item">
+
+										  <a href=""><img src="images/product-details/sastreformal2piezas/miniaturas/View3.jpg" alt=""></a>
+
+										  <a href=""><img src="images/product-details/sastreformal2piezas/miniaturas/View2.jpg" alt=""></a>
+
+										  <a href=""><img src="images/product-details/sastreformal2piezas/miniaturas/View1.jpg" alt=""></a>
+
+										  <a href=""><img src="images/product-details/sastreformal2piezas/miniaturas/View4.jpg" alt=""></a>
+
+										</div>
+
+										<div class="item">
+
+										  <a href=""><img src="images/product-details/sastreformal2piezas/miniaturas/View3.jpg" alt=""></a>
+
+										  <a href=""><img src="images/product-details/sastreformal2piezas/miniaturas/View2.jpg" alt=""></a>
+
+										  <a href=""><img src="images/product-details/sastreformal2piezas/miniaturas/View1.jpg" alt=""></a>
+
+										  <a href=""><img src="images/product-details/sastreformal2piezas/miniaturas/View4.jpg" alt=""></a>
+
+										</div>-->
+
+										
+
+									</div>
+
+
+
+								  <!-- Controls -->
+
+								  <!--<a class="left item-control" href="#similar-product" data-slide="prev">
+
+									<i class="fa fa-angle-left"></i>
+
+								  </a>
+
+								  <a class="right item-control" href="#similar-product" data-slide="next">
+
+									<i class="fa fa-angle-right"></i>
+
+								  </a>-->
 
 							</div>
 
@@ -220,39 +250,54 @@
 
 							<div class="product-information"><!--/product-information-->
 
-								<img src="images/product-details/new.jpg" class="newarrival" alt="" />
+								<!--<img src="images/product-details/new.jpg" class="newarrival" alt="" />-->
 
-								<h2>Anne Klein Sleeveless Colorblock Scuba</h2>
+								<h2>Sastre Formal De Dos (2) Piezas</h2>
 
 								<p>Web ID: 1089772</p>
 
-								<img src="images/product-details/rating.png" alt="" />
+								<!--<img src="images/product-details/rating.png" alt="" />-->
 
 								<span>
 
-									<span>US $59</span>
+									<!--<span>US $59</span>-->
 
-									<label>Quantity:</label>
+									<label>Cantidad:</label>
 
-									<input type="text" value="3" />
+									<input type="text" value="1" />
 
 									<button type="button" class="btn btn-fefault cart">
 
 										<i class="fa fa-shopping-cart"></i>
 
-										Add to cart
+										Agregar al Carrito
 
 									</button>
 
 								</span>
 
-								<p><b>Availability:</b> In Stock</p>
+								<p><b>Disponible:</b> En Stock</p>
 
-								<p><b>Condition:</b> New</p>
+								<p><b>Talla</b></p>
+								<select class="col-sm-3" name="color">
+								    <option value="S">XS</option>
+								    <option value="S">S</option>
+								    <option value="M">M</option>
+								    <option value="L">L</option>
+								    <option value="XL">XL</option>
+								    <option value="2XL">2XL</option>
+								    <option value="3XL">3XL</option>
+								 </select>
+								<br></br>
+								<p><b>Color</b></p>
+								<select class="col-sm-3" name="color">
+								    <option value="cafe">Café</option>
+								    <option value="Negro">Negro</option>
+								    <option value="Vinotinto">Vinotinto</option>
+								    <option value="Azul">Azul</option>
+								 </select>
 
-								<p><b>Brand:</b> E-SHOPPER</p>
-
-								<a href=""><img src="images/product-details/share.png" class="share img-responsive"  alt="" /></a>
+								<!--<a href=""><img src="images/product-details/share.png" class="share img-responsive"  alt="" /></a>-->
 
 							</div><!--/product-information-->
 
@@ -268,13 +313,13 @@
 
 							<ul class="nav nav-tabs">
 
-								<li><a href="#details" data-toggle="tab">Details</a></li>
+								<!--<li><a href="#details" data-toggle="tab">Details</a></li>
 
 								<li><a href="#companyprofile" data-toggle="tab">Company Profile</a></li>
 
-								<li><a href="#tag" data-toggle="tab">Tag</a></li>
+								<li><a href="#tag" data-toggle="tab">Tag</a></li>-->
 
-								<li class="active"><a href="#reviews" data-toggle="tab">Reviews (5)</a></li>
+								<li class="active"><a href="#reviews" data-toggle="tab">Detalles</a></li>
 
 							</ul>
 
@@ -282,7 +327,7 @@
 
 						<div class="tab-content">
 
-							<div class="tab-pane fade" id="details" >
+							<!--<div class="tab-pane fade" id="details" >
 
 								<div class="col-sm-3">
 
@@ -584,7 +629,7 @@
 
 								</div>
 
-							</div>
+							</div>-->
 
 							
 
@@ -594,21 +639,23 @@
 
 									<ul>
 
-										<li><a href=""><i class="fa fa-user"></i>EUGEN</a></li>
+										<li><i class="fa fa-user"></i>Arotex Confecciones</a></li>
 
-										<li><a href=""><i class="fa fa-clock-o"></i>12:41 PM</a></li>
+										<li><i class="fa fa-clock-o"></i>Utima Actualización 12:41 PM</a></li>
 
-										<li><a href=""><i class="fa fa-calendar-o"></i>31 DEC 2014</a></li>
+										<li><i class="fa fa-calendar-o"></i>03 OCT 2016</a></li>
 
 									</ul>
 
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+									<!--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 
-									<p><b>Write Your Review</b></p>
+									<!--<p><b>Write Your Review</b></p>-->
 
-									
+									<img src="images/product-details/tallas1.jpg" alt="" />
 
-									<form action="#">
+									<img src="images/product-details/tallas2.jpg" alt="" />
+
+									<!--<form action="#">
 
 										<span>
 
@@ -628,7 +675,7 @@
 
 										</button>
 
-									</form>
+									</form>-->
 
 								</div>
 
