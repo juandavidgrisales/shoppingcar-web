a87a2afb2864b37ac92bb9483c354b91dfd132bd
Jefferson Gaviria Aranzalez <Jefferson Gaviria Aranzalez>
2016-11-09 19:30:14 -0500

    pestañas agregadas

---
+++ guia-uso.php
@@ -0,0 +1,32 @@
+﻿<?php
+	include('header.php');
+?>
+	<section id="guia-uso"><!--form-->
+		<div class="container">
+			<div class="breadcrumbs">
+				<ol class="breadcrumb">
+				  <li><a href="index.php">Home</a></li>
+				  <li class="active"><b>Guia Uso</b></li>
+				  <h3>Guia de uso de la plataforma</h3>
+				  <div align="justify">
+						<p>A continuación encontrara los pasos para un buen uso del sitio web y que le pueden servir para diligenciar su orden:</p>
+						<p>1.	El beneficiario,  Entidad Compradora o Supervisor se deben identificar en el área “Inicia Sesión” con las respectivas credenciales adquiridas previamente entregadas por AROTEX CONFECCIONES o el personal encargado.</p>
+						<p>2.	Después de ingresar e identificarse, la plataforma automáticamente identifica el tipo de usuario (Rol).</p>
+						<p>3.	El usuario adquiere permisos para ver únicamente los productos para los cuales está autorizado, en el caso del rol Supervisor, el usuario Supervisor solo podrá ver la información de los beneficiarios que ya han redimido su orden y los que aun no lo han hecho. También pobra ver el total de órdenes autorizadas.</p>
+						<p>4.	Luego de que el usuario (Beneficiario) identifica al costado izquierdo los productos para los cuales está autorizado en sus respectivos segmentos, entonces agrega las cantidades que desea (Las cantidades posteriormente pueden ser editas) y agrega al carrito.</p>
+						<p>5.	En la parte inferior de los productos podrá encontrar un menú con los detalles de materiales, guía de tallas y descripción.</p>
+						<p>6.	Luego de enviar todos los productos al carrito, procede a ir al carrito de compras y ver todos los productos elegidos y sus cantidades. Hay que tener en cuenta que la plataforma solo dejara escoger los productos y cantidades que previamente le fueron autorizados. En caso contrario una ventana emergente le avisara al usuario comprador que las cantidades solicitadas no corresponden a la orden entregada. En caso de que el usuario desee puede editar su compra y volver a la tienda a agregar un nuevo producto.</p>
+						<p>7.	Después vendrá una pantalla llamada “Checkout” en donde  el usuario diligencia sus datos de envió (Ciudad, dirección y teléfono)  y si lo desea puede adjuntar una nota con algunos detalles e información adicional.</p>
+						<p>8.	En el botón “Confirmar” termina el proceso de diligenciamiento de la orden la cual es enviada y procederemos en su despacho.</p>
+						<p>9.	En caso de necesitas ayuda, puede entrar al área de contacto y comunicarse con nosotros.</p>
+					</div>
+				</ol>
+			</div>
+			<br><a class="btn btn-primary" href="index.php">Volver al Home</a>
+			<br></br>
+		</div>
+	</section><!--/form-->
+
+<?php	
+	include('footer.php');
+?>
---
+++ mapa-virtual.php
@@ -0,0 +1,21 @@
+<?php
+	include('header.php');
+?>
+	<section id="mapa-virtual"><!--form-->
+		<div class="container">
+			<div class="breadcrumbs">
+				<ol class="breadcrumb">
+				  <li><a href="index.php">Home</a></li>
+				  <li class="active"><b>Mapa Virtual</b></li>
+				  <h2>Mapa Virtual</h2>
+				  <img src="images/home/mapapuntovirtual.jpg" alt="" />
+				</ol>
+			</div>
+			<br><a class="btn btn-primary" href="index.php">Volver al Home</a>
+			<br></br>
+		</div>
+	</section><!--/form-->
+
+<?php	
+	include('footer.php');
+?>
